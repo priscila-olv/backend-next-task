7400f943622d9fb312221620b42f8fac5c380869
PRISCILA DE OLIVEIRA <priscila_oliveira5@estudante.sesisenai.org.br>
2023-05-30 19:42:14 -0300

    Novas validações de usuarios

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Models\Project;
@@ -13,6 +14,7 @@ class TasksController extends Controller
     {
         $this->middleware('jwt.auth');
     }
+
     public function index()
     {
         try {
@@ -26,7 +28,7 @@ class TasksController extends Controller
     {
         try {
             $user_id = auth()->id();
-    
+
             $validatedData = $request->validate([
                 'name' => 'required',
                 'description' => 'nullable',
@@ -35,19 +37,19 @@ class TasksController extends Controller
                 'section_id' => 'required|exists:sections,id',
                 'priority_id' => 'required|exists:priorities,id'
             ]);
-    
+
             $section = Section::findOrFail($validatedData['section_id']);
             $project = $section->project;
-    
+
             $userProject = DB::table('user_projects')
                 ->where('user_id', $user_id)
                 ->where('project_id', $project->id)
                 ->first();
-    
+
             if (!$userProject) {
                 return response()->json(['error' => 'Você não tem permissão para criar tarefas nesse projeto.'], 403);
             }
-    
+
             $task = Task::create([
                 'name' => $validatedData['name'],
                 'description' => $validatedData['description'],
@@ -56,39 +58,49 @@ class TasksController extends Controller
                 'sections_id' => $validatedData['section_id'],
                 'priorities_id' => $validatedData['priority_id']
             ]);
-    
+
             return response()->json(['task' => $task, 'message' => 'Tarefa criada com sucesso.'], 201);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-    
+
     public function remove(Task $task)
     {
         try {
             $user_id = auth()->id();
-            $section = Section::find($task->sections_id);
+            $section = Section::findOrFail($task->sections_id);
             $project = Project::find($section->projects_id);
 
-            if ($project->users_id != $user_id) {
+            $userProject = DB::table('user_projects')
+                ->where('user_id', $user_id)
+                ->where('project_id', $project->id)
+                ->first();
+
+            if (!$userProject) {
                 return response()->json(['error' => 'Você não tem permissão para remover tarefas desse projeto.'], 403);
             }
 
             $task->delete();
             return response()->json(['message' => 'Tarefa removida com sucesso!'], 200);
-
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+
     public function getByIdTask(Task $task)
     {
         try {
             $user_id = auth()->id();
-            $section = Section::find($task->sections_id);
+            $section = Section::findOrFail($task->sections_id);
             $project = Project::find($section->projects_id);
 
-            if ($project->users_id != $user_id) {
+            $userProject = DB::table('user_projects')
+            ->where('user_id', $user_id)
+            ->where('project_id', $project->id)
+            ->first();
+
+            if (!$userProject) {
                 return response()->json(['error' => 'Você não tem permissão para visualizar esta tarefa.'], 403);
             }
 
@@ -97,65 +109,71 @@ class TasksController extends Controller
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+
     public function getByIdSection(Section $section)
     {
         try {
             $user_id = auth()->id();
             $project = Project::find($section->projects_id);
-    
-            if ($project->users_id != $user_id) {
+
+            $userProject = DB::table('user_projects')
+            ->where('user_id', $user_id)
+            ->where('project_id', $project->id)
+            ->first();
+
+            if (!$userProject) {
                 return response()->json(['error' => 'Você não tem permissão para visualizar as tarefas desta seção.'], 403);
             }
-    
+
             $tasks = Task::where('sections_id', $section->id)->get();
-    
+
             return response()->json(['tasks' => $tasks], 200);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+
     public function update(Request $request, Task $task)
     {
         try {
             $user_id = auth()->id();
-            $section = Section::find($task->sections_id);
+            $section = Section::findOrFail($task->sections_id);
             $project = Project::find($section->projects_id);
-    
-            if ($project->users_id != $user_id) {
-                return response()->json(['error' => 'Você não tem permissão para atualizar esta tarefa.'], 403);
+
+            $userProject = DB::table('user_projects')
+            ->where('user_id', $user_id)
+            ->where('project_id', $project->id)
+            ->first();
+
+            if (!$userProject) {
+                return response()->json(['error' => 'Você não tem permissão para visualizar esta tarefa.'], 403);
             }
-    
+
             $sectionId = $request->input('section_id');
             $priorityId = $request->input('priority_id');
-    
+
             $section = Section::find($sectionId);
             if (!$section) {
                 return response()->json(['error' => 'A seção selecionada não existe.'], 404);
             }
-    
-            $project = Project::find($section->projects_id);
-    
-            if ($project->users_id != $user_id) {
-                return response()->json(['error' => 'Você não tem permissão para atualizar esta tarefa para a seção selecionada.'], 403);
-            }
-    
+
             $task->fill($request->only([
                 'name',
                 'description',
                 'color',
                 'expiration_date'
             ]));
-    
+
             if ($sectionId) {
                 $task->sections_id = $sectionId;
             }
-    
+
             if ($priorityId) {
                 $task->priorities_id = $priorityId;
             }
-    
+
             $task->save();
-    
+
             return response()->json(['task' => $task, 'message' => 'Tarefa atualizada com sucesso.'], 200);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
